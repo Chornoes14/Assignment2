957f67f8beb06da146efd5d91b9e21ce14abe73b
Chornoes <chornjulian@gmail.com>
2025-02-05 13:21:40 +1100

    relocating files, no main changes to code

--- test/src/Controller.java
+++ test/src/Controller.java
@@ -1,4 +1,4 @@
-package hellofx;
+
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- test/src/Main.java
+++ test/src/Main.java
@@ -1,4 +1,4 @@
-package hellofx;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
